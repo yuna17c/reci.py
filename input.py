00abde56ce22737efa1d9c2502ab9a7d12fb4b98
Josephine Widjaja <75404368+josephine-w@users.noreply.github.com>
2022-01-13 22:27:58 -0500

    Update input.py
    Add option to edit ingredient list

--- input.py
+++ input.py
@@ -1,3 +1,6 @@
+def printIngredients(ingredients): 
+    print("Your ingredients are: ")
+    print(*ingredients, sep = ", ") 
 
 ingredients = []
 while True:
@@ -6,6 +9,29 @@ while True:
         break
     ingredients.append(user_input)
 
-print("Your ingredients are: ")
-print(*ingredients, sep = ", ") 
+printIngredients(ingredients)
+
+edit = False
+edit_input = input("Would you like to add/remove ingredients? (Y/N): ")
+
+if (edit_input == 'Y'):
+    edit = True
+    print("Type 'add ____' or 'remove ____' to add/remove ingredients. Otherwise type 'done'.")
+
+while edit:
+    user_input = input()
+    if user_input == 'done':
+        printIngredients(ingredients)
+        break
+    if user_input[0:3].lower() == "add" and (user_input[4:] not in ingredients):
+        ingredients.append(user_input[4:])
+        printIngredients(ingredients)
+    elif user_input[0:6].lower() == "remove" and (user_input[7:] in ingredients):
+        ingredients.remove(user_input[7:])
+        printIngredients(ingredients)
+    else:
+        print("Invalid input. Try again. :3")
+    
+        
+
 
