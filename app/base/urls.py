58997fdd20312dc1eb2c7d47f0b947bacdeefa68
yuna17c <yuna0617c@gmail.com>
2022-01-15 21:55:41 -0500

    Add delete/edit ingredients

--- app/base/urls.py
+++ app/base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import FridgeHome, HomePage, RecipeFinderHome
+from .views import FridgeHome, HomePage, RecipeFinderHome, DeleteView
 
 urlpatterns = [
     path('', HomePage.as_view(), name='main'),
     path('fridge', FridgeHome.as_view(), name='fridge'),
     path('recipefinder', RecipeFinderHome.as_view(), name='recipe-finder'),
+    path('ingredient-update/<int:pk>/', DeleteView.as_view(), name='ingredient-update'),
 ]
--- app/base/views.py
+++ app/base/views.py
@@ -1,10 +1,11 @@
 from msilib.schema import ListView
 from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Ingredient
 
@@ -15,6 +16,10 @@ class HomePage(ListView):
 class RecipeFinderHome(ListView):
     model = Ingredient
     template_name = "base/recipe_home.html"
+    def get_context_data(self, **kwargs):
+        all_entries = Ingredient.objects.all()
+        context = {'ingredients' : all_entries}
+        return context
     def post(self, request):
         user_input = request.POST.get("input", "")
         print(user_input)
@@ -24,6 +29,13 @@ class RecipeFinderHome(ListView):
         for a in all_entries:
             print(a.name)
         return render(request, "base/recipe_home.html", context)
+
+class DeleteView(DeleteView):
+    model = Ingredient
+    context_object_name = 'ingredient'
+    success_url = reverse_lazy('recipe-finder')
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 
 class FridgeHome(ListView):
