38add7eb5202011f856ceb37dac0cd76b1c467b6
Josephine Widjaja <75404368+josephine-w@users.noreply.github.com>
2022-01-26 17:26:50 -0500

    change directory name and update fridge model

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,5 +1,17 @@
+import django
 from django.db import models
+from django.utils import timezone
 
+FOOD_GROUPS = (
+    ("fruits", "fruits/vegetables"),
+    ("fats", "fats + oils"),
+    ("dairy", "eggs, milk and milk products"),
+    ("meat", "meat + fish"),
+    ("herbs", "herbs + spices"),
+    ("grain", "grain, nuts and baking products"),
+    ("pasta", "pasta, rice and pulses"),
+    ("other", "other")
+)
 # Create your models here.
 class Ingredient(models.Model):
     name = models.TextField(null=True, blank=True)
@@ -12,6 +24,7 @@ class Ingredient(models.Model):
 class FoodItem(models.Model):
     name = models.TextField(null=True, blank=False)
     expiry_date = models.DateField(null=True, blank=False)
+    food_group = models.TextField(choices=FOOD_GROUPS, default="other")
     def __str__(self):
         return self.name
 
