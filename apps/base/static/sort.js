af8696e65517c3493203ec33110f97a372ed43d5
Josephine Widjaja <75404368+josephine-w@users.noreply.github.com>
2022-02-06 02:06:37 -0500

    set up sorting for table (doesn't work yet lol)

---
+++ apps/base/static/sort.js
@@ -0,0 +1,12 @@
+function formatDate(date) {
+    var date_array=date.split("/");
+    return (date_array[2]+date_array[0]+date_array[2]);
+}
+
+function sortByName() {
+    
+}
+
+function sortByDate() {
+    
+}
