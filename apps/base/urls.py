9e23dd3c76bc6bdc7254d5c3d0859fba946eb4f7
yuna17c <yuna0617c@gmail.com>
2022-03-14 02:42:03 -0400

    Remove debug tools

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('pantry/', PantryHome.as_view(), name='pantry'),
     path('recipefinder/', RecipeFinderHome.as_view(), name='recipe-finder'),
     path('ingredient-update/<int:pk>/', DeleteView.as_view(), name='ingredient-update'),
-    path('pantry-update/<int:pk>/', DeletePantryView.as_view(), name='pantry-update'),
-    path('__debug__/', include('debug_toolbar.urls'))
+    path('pantry-update/<int:pk>/', DeletePantryView.as_view(), name='pantry-update')
 ]
